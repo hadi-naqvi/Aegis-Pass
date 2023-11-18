5a3f327630a652e9121265b2caf20134d10ecef3
Josh <josh.joseph@mail.utoronto.ca>
2023-11-18 17:34:11 -0500

    Implemented create method in SetupAuthUseCaseFactory

--- src/app/SetupAuthUseCaseFactory.java
+++ src/app/SetupAuthUseCaseFactory.java
@@ -1,4 +1,32 @@
 package app;
 
+import interface_adapter.SetupAuth.SetupAuthViewModel;
+import interface_adapter.SetupAuth.SetupAuthController;
+import interface_adapter.SetupAuth.SetupAuthPresenter;
+import use_case.SetupAuth.SetupAuthDataAccessInterface;
+import entity.CommonAuthKey;
+import entity.AuthKey;
+import interface_adapter.*;
+import use_case.SetupAuth.SetupAuthInputBoundary;
+import use_case.SetupAuth.SetupAuthUseCaseInteractor;
+import use_case.SetupAuth.SetupAuthOutputBoundary;
+import view.SetupAuthView;
+
+import javax.swing.*;
+import java.io.IOException;
 public class SetupAuthUseCaseFactory {
+
+    private SetupAuthUseCaseFactory() {}
+
+    public static SetupAuthView create(
+            ViewManagerModel viewManagerModel, SetupAuthViewModel setupAuthViewModel, SetupAuthDataAccessInterface userDataAccessObject){
+
+        try{
+            SetupAuthController setupAuthController = createSetupAuthUseCase(viewManagerModel, setupAuthViewModel, userDataAccessObject);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Could not open user data file.");
+        }
+
+        return null;
+    }
 }
