51ef6b6d912b8f3b747e98416513e23ec926f4d9
Hadi-Naqvi <hadinaqvi04@gmail.com>
2023-11-18 18:20:32 -0500

    Added partially complete methods in AuthenticationViewModel needed for SetupAuth use case

--- src/interface_adapter/Authentication/AuthenticationViewModel.java
+++ src/interface_adapter/Authentication/AuthenticationViewModel.java
@@ -2,5 +2,23 @@ package interface_adapter.Authentication;
 
 import interface_adapter.ViewModel;
 
+import java.beans.PropertyChangeListener;
+
 public class AuthenticationViewModel extends ViewModel {
+    /**
+     * Constructor method for the Authentication view's view model
+     */
+    public AuthenticationViewModel() {
+        super("authentication");
+    }
+
+    @Override
+    public void firePropertyChanged() {
+        
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+
+    }
 }
