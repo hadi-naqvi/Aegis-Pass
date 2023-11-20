5412cd6bb32515da447f9ff3e42351aa05021ad0
hadi-naqvi <92334527+hadi-naqvi@users.noreply.github.com>
2023-11-20 17:14:10 -0500

    Implemented input and output data classes for the Authentication use case

--- src/use_case/Authentication/AuthenticationInputData.java
+++ src/use_case/Authentication/AuthenticationInputData.java
@@ -1,2 +1,20 @@
-package use_case.Authentication;public class AuthenticationInputData {
+package use_case.Authentication;
+public class AuthenticationInputData {
+    final private String password;
+
+    /**
+     * Constructor method for the Authentication use case's input data
+     * @param password The password being entered during authentication
+     */
+    public AuthenticationInputData(String password) {
+        this.password = password;
+    }
+
+    /**
+     * Getter method for the password being entered during authentication
+     * @return The password being entered during authentication
+     */
+    public String getPassword() {
+        return this.password;
+    }
 }
--- src/use_case/Authentication/AuthenticationOutputData.java
+++ src/use_case/Authentication/AuthenticationOutputData.java
@@ -1,2 +1,13 @@
-package use_case.Authentication;public class AuthenticationOutputData {
+package use_case.Authentication;
+
+public class AuthenticationOutputData {
+    private boolean useCaseFailed;
+
+    /**
+     * Consturctor method for the authentication output data
+     * @param useCaseFailed Whether the use case failed or not (i.e. failed authentication)
+     */
+    public AuthenticationOutputData(boolean useCaseFailed) {
+        this.useCaseFailed = useCaseFailed;
+    }
 }
