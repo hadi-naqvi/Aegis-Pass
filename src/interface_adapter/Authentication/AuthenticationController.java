6089b7e15541e474bd5fdeef90ad6b95eb26c81b
hadi-naqvi <92334527+hadi-naqvi@users.noreply.github.com>
2023-11-20 17:16:35 -0500

    Implemented the controller for the authentication use case

--- src/interface_adapter/Authentication/AuthenticationController.java
+++ src/interface_adapter/Authentication/AuthenticationController.java
@@ -1,4 +1,25 @@
 package interface_adapter.Authentication;
 
+import use_case.Authentication.AuthenticationInputBoundary;
+import use_case.Authentication.AuthenticationInputData;
+
 public class AuthenticationController {
+    public final AuthenticationInputBoundary authenticationInteractor;
+
+    /**
+     * Constructor method for the controller for the authentication use case
+     * @param authenticationInteractor The use case interactor object for the authentication use case
+     */
+    public AuthenticationController(AuthenticationInputBoundary authenticationInteractor) {
+        this.authenticationInteractor = authenticationInteractor;
+    }
+
+    /**
+     * Method which is executed/triggered when the user enters their authentication password
+     * @param password The password/authentication key being entered by the user
+     */
+    public void execute(String password) {
+        AuthenticationInputData authenticationInputData = new AuthenticationInputData(password);
+        authenticationInteractor.execute(authenticationInputData);
+    }
 }
