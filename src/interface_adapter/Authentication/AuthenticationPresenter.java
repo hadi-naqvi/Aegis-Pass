c97cdcc893fed42ec0292307d59140d50b6368b7
hadi-naqvi <92334527+hadi-naqvi@users.noreply.github.com>
2023-11-20 17:16:47 -0500

    Implemented the presenter for the authentication use case

--- src/interface_adapter/Authentication/AuthenticationPresenter.java
+++ src/interface_adapter/Authentication/AuthenticationPresenter.java
@@ -1,4 +1,48 @@
 package interface_adapter.Authentication;
 
-public class AuthenticationPresenter {
+import interface_adapter.ViewManagerModel;
+import use_case.Authentication.AuthenticationOutputBoundary;
+import use_case.Authentication.AuthenticationOutputData;
+
+public class AuthenticationPresenter implements AuthenticationOutputBoundary {
+    private final AuthenticationViewModel authenticationViewModel;
+    private final DashboardViewModel dashboardViewModel;
+    private final ViewManagerModel viewManagerModel;
+
+    /**
+     * Constructor method for the Authentication use case's presenter
+     * @param authenticationViewModel The view model for the authentication view
+     * @param dashboardViewModel The view model for the dashboard view
+     */
+    public AuthenticationPresenter(ViewManagerModel viewManagerModel,
+                                   AuthenticationViewModel authenticationViewModel,
+                                   DashboardViewModel dashboardViewModel) {
+        this.authenticationViewModel = authenticationViewModel;
+        this.dashboardViewModel = dashboardViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+
+    /**
+     * Method which updates the view manager model to display the dashboard screen after setup is complete
+     * @param authenticationOutputData The Authentication use case output data
+     */
+    @Override
+    public void prepareSuccessView(AuthenticationOutputData authenticationOutputData) {
+        DashboardState dashboardState = dashboardViewModel.getState();
+        this.dashboardViewModel.setState(dashboardState);
+
+        this.viewManagerModel.setActiveView(dashboardViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
+    }
+
+    /**
+     * Method which updates the view manager model to display the failed Authentication screen
+     * @param error The error message which explains why Authentication failed
+     */
+    @Override
+    public void prepareFailView(String error) {
+        AuthenticationState authenticationState = authenticationViewModel.getState();
+        authenticationState.setPasswordError(error);
+        authenticationViewModel.firePropertyChanged();
+    }
 }
