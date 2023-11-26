f7ad9ecc9a036a20f5e29b64635aeefa313aaa49
hadi-naqvi <92334527+hadi-naqvi@users.noreply.github.com>
2023-11-25 21:36:56 -0500

    Added javadocs/documentation to SetupAuth and Authentication use case factories

--- src/app/AuthenticationUseCaseFactory.java
+++ src/app/AuthenticationUseCaseFactory.java
@@ -1,24 +1,27 @@
 package app;
 
-import interface_adapter.Authentication.AuthenticationViewModel;
-import interface_adapter.DisplayDash.DashboardViewModel;
-import interface_adapter.SetupAuth.SetupAuthViewModel;
 import interface_adapter.Authentication.AuthenticationController;
 import interface_adapter.Authentication.AuthenticationPresenter;
+import interface_adapter.Authentication.AuthenticationViewModel;
+import interface_adapter.DisplayDash.DashboardViewModel;
+import interface_adapter.ViewManagerModel;
 import use_case.Authentication.AuthenticationDataAccessInterface;
-import use_case.Authentication.*;
-import entity.CommonAuthKey;
-import entity.AuthKey;
-import interface_adapter.*;
+import use_case.Authentication.AuthenticationInputBoundary;
+import use_case.Authentication.AuthenticationInteractor;
+import use_case.Authentication.AuthenticationOutputBoundary;
 import view.AuthenticationView;
-import view.SetupAuthView;
 
 import javax.swing.*;
 import java.io.IOException;
 public class AuthenticationUseCaseFactory {
-
-    private AuthenticationUseCaseFactory() {}
-
+    /**
+     * Method which creates a new Authentication view by instantiating all necessary interface adapters
+     * @param viewManagerModel The view manager model
+     * @param authenticationViewModel The view model for the authentication view
+     * @param dashboardViewModel The view model for the dashboard view
+     * @param userDataAccessObject The data access object authentication/setupAuth
+     * @return A new Authentication view
+     */
     public static AuthenticationView create(
             ViewManagerModel viewManagerModel, AuthenticationViewModel authenticationViewModel, DashboardViewModel dashboardViewModel, AuthenticationDataAccessInterface userDataAccessObject){
 
@@ -32,6 +35,14 @@ public class AuthenticationUseCaseFactory {
         return null;
     }
 
+    /**
+     * Method which creates and returns a new controller object for the Authentication use case
+     * @param viewManagerModel The view manager model
+     * @param authenticationViewModel The view model for the authentication view
+     * @param dashboardViewModel The view model for the dashboard view
+     * @param userDataAccessObject The data access object for authentication/setupAuth
+     * @return A new controller for the Authentication use case
+     */
     private static AuthenticationController createAuthenticationUseCase(ViewManagerModel viewManagerModel, AuthenticationViewModel authenticationViewModel, DashboardViewModel dashboardViewModel,
                                                               AuthenticationDataAccessInterface userDataAccessObject) throws IOException{
         AuthenticationOutputBoundary authenticationPresenter = new AuthenticationPresenter(viewManagerModel, authenticationViewModel, dashboardViewModel);
--- src/app/SetupAuthUseCaseFactory.java
+++ src/app/SetupAuthUseCaseFactory.java
@@ -1,21 +1,27 @@
 package app;
 
 import interface_adapter.Authentication.AuthenticationViewModel;
-import interface_adapter.SetupAuth.SetupAuthViewModel;
 import interface_adapter.SetupAuth.SetupAuthController;
 import interface_adapter.SetupAuth.SetupAuthPresenter;
-import use_case.SetupAuth.*;
-import entity.CommonAuthKey;
-import entity.AuthKey;
-import interface_adapter.*;
+import interface_adapter.SetupAuth.SetupAuthViewModel;
+import interface_adapter.ViewManagerModel;
+import use_case.SetupAuth.SetupAuthDataAccessInterface;
+import use_case.SetupAuth.SetupAuthInputBoundary;
+import use_case.SetupAuth.SetupAuthInteractor;
+import use_case.SetupAuth.SetupAuthOutputBoundary;
 import view.SetupAuthView;
 
 import javax.swing.*;
 import java.io.IOException;
 public class SetupAuthUseCaseFactory {
-
-    //private SetupAuthUseCaseFactory() {}
-
+    /**
+     * Method which creates a new SetupAuth view by instantiating all necessary interface adapters
+     * @param viewManagerModel The view manager model
+     * @param setupAuthViewModel The view model for the setup auth view
+     * @param authenticationViewModel The view model for the authentication view
+     * @param userDataAccessObject The data access object for authentication/setupAuth
+     * @return A new setup auth view
+     */
     public static SetupAuthView create(
             ViewManagerModel viewManagerModel, SetupAuthViewModel setupAuthViewModel,
             AuthenticationViewModel authenticationViewModel, SetupAuthDataAccessInterface userDataAccessObject){
@@ -31,6 +37,14 @@ public class SetupAuthUseCaseFactory {
         return null;
     }
 
+    /**
+     * Method which creates and returns a new controller object for the Authentication use case
+     * @param viewManagerModel The view manager model
+     * @param setupAuthViewModel The view model for the setup auth view
+     * @param authenticationViewModel The view model for the authentication view
+     * @param userDataAccessObject The data access object for authentication/setupAuth
+     * @return A new controller for the setup auth use case
+     */
     private static SetupAuthController createSetupAuthUseCase(ViewManagerModel viewManagerModel, SetupAuthViewModel setupAuthViewModel,
                                                               AuthenticationViewModel authenticationViewModel,
                                                               SetupAuthDataAccessInterface userDataAccessObject) throws IOException{
