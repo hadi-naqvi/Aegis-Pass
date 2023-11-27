dc6c85b62a5b166954401a809c6db139ae8c3ebe
Josh <josh.joseph@mail.utoronto.ca>
2023-11-27 13:57:39 -0500

    Updated AuthenticationDAI with username paramater

--- src/use_case/Authentication/AuthenticationDataAccessInterface.java
+++ src/use_case/Authentication/AuthenticationDataAccessInterface.java
@@ -1,4 +1,14 @@
 package use_case.Authentication;
 
 public interface AuthenticationDataAccessInterface {
+    /**
+     * Method validates the user's password with their corresponding username
+     * @param username The user's inputted username
+     * @param password The user's inputted password
+     * @return returns whether password matches username
+     */
+    boolean validate(String username, String password);
+
+
+
 }
