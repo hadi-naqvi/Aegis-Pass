3d5ad77ba5fa321559ee75ce72f9a15144fd9bd5
Josh <josh.joseph@mail.utoronto.ca>
2023-11-27 14:02:11 -0500

    Updated AuthenticationInputData  with username paramater

--- src/use_case/Authentication/AuthenticationInputBoundary.java
+++ src/use_case/Authentication/AuthenticationInputBoundary.java
@@ -1,4 +1,8 @@
 package use_case.Authentication;
 public interface AuthenticationInputBoundary {
+    /**
+     * Method which contains the logic for the Authentication use case (interactor) which is triggered to complete use case
+     * @param authenticationInputData The input data for the use case interactor
+     */
     void execute(AuthenticationInputData authenticationInputData);
 }
--- src/use_case/Authentication/AuthenticationInputData.java
+++ src/use_case/Authentication/AuthenticationInputData.java
@@ -1,13 +1,23 @@
 package use_case.Authentication;
 public class AuthenticationInputData {
+    final private String username;
     final private String password;
 
     /**
      * Constructor method for the Authentication use case's input data
      * @param password The password being entered during authentication
      */
-    public AuthenticationInputData(String password) {
+    public AuthenticationInputData(String username, String password) {
+        this.username = username;
         this.password = password;
+    }
+
+    /**
+     * Getter method for the username being entered during authentication
+     * @return The username being entered during authentication
+     */
+    public String getUsername() {
+        return this.username;
     }
 
     /**
