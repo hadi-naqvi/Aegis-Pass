800bafa4f66106b91d98a8394e0e5bf65d1b0434
hadi-naqvi <92334527+hadi-naqvi@users.noreply.github.com>
2023-12-01 18:18:39 -0500

    Implemented the controller for the displaydash use case

--- src/interface_adapter/Dashboard/DashboardController.java
+++ src/interface_adapter/Dashboard/DashboardController.java
@@ -1,5 +1,25 @@
 package interface_adapter.Dashboard;
 
-public class DashboardController {
+import use_case.Dashboard.DashboardInputBoundary;
+import use_case.Dashboard.DashboardInputData;
 
+public class DashboardController {
+    public final DashboardInputBoundary authenticationInteractor;
+
+    /**
+     * Constructor method for the controller for the authentication use case
+     * @param dashboardInteractor The use case interactor object for the displayDash use case
+     */
+    public DashboardController(DashboardInputBoundary dashboardInteractor) {
+        this.authenticationInteractor = dashboardInteractor;
+    }
+
+    /**
+     * Method which is executed/triggered when the user enters the dashbaord view
+     * @param userID The currently signed in user's ID
+     */
+    public void execute(int userID) {
+        DashboardInputData dashboardInputData = new DashboardInputData(userID);
+        authenticationInteractor.execute(dashboardInputData);
+    }
 }
