7dc7cde6ee03b5bd08ca4cba25f2215f58e4af38
hadi-naqvi <92334527+hadi-naqvi@users.noreply.github.com>
2023-12-01 16:59:17 -0500

    Created/implemented entity class for AccountInfo factory

--- src/entity/CommonAccountInfoFactory.java
+++ src/entity/CommonAccountInfoFactory.java
@@ -1,4 +1,24 @@
 package entity;
 
-public class CommonAccountInfoFactory {
+import java.util.Date;
+
+public class CommonAccountInfoFactory implements AccountInfoFactory {
+    /**
+     * Factory method which creates and returns a new AccountInfo object given all its data
+     *
+     * @param title     The title of the account
+     * @param username  The username of the account
+     * @param password  The password of the account
+     * @param secretKey The 2FA secret key of the account
+     * @param url       The URL of the account
+     * @param iconURL   The URL for the icon of the account
+     * @param date      The last modified date of the account
+     * @param notes     The notes for the account
+     * @return A new AccountInfo object that stores the account details
+     */
+    @Override
+    public AccountInfo create(String title, String username, String password, String secretKey, String url,
+                              String iconURL, Date date, String notes) {
+        return new CommonAccountInfo(title, username, password, secretKey, url, iconURL, date, notes);
+    }
 }
