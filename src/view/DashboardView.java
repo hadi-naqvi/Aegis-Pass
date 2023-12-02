81e1870277e0bae53b469f500963929789d9e670
Josh <josh.joseph@mail.utoronto.ca>
2023-12-01 23:12:39 -0500

    Implemented core components in DashboardView(JTable and adding useraccounts to table)

--- src/view/DashboardView.java
+++ src/view/DashboardView.java
@@ -1,4 +1,62 @@
 package view;
 
-public class DashboardView {
+import entity.AccountInfo;
+import interface_adapter.Dashboard.DashboardController;
+import interface_adapter.Dashboard.DashboardState;
+import interface_adapter.Dashboard.DashboardViewModel;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.List;
+
+public class DashboardView extends JPanel implements ActionListener, PropertyChangeListener {
+
+    public final String viewName = "display dash";
+    private final DashboardController dashboardController;
+    private final DashboardViewModel dashboardViewModel;
+    private JButton mainView;
+    private DefaultTableModel defaultTableModel;
+    private JTable accounts;
+
+    public DashboardView(DashboardViewModel dashboardViewModel, DashboardController dashboardController){
+        this.dashboardViewModel = dashboardViewModel;
+        this.dashboardController = dashboardController;
+        this.dashboardViewModel.addPropertyChangeListener(this);
+
+
+
+    }
+
+    /**
+     * Invoked when an action occurs.
+     *
+     * @param e the event to be processed
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+
+    /**
+     * This method gets called when a bound property is changed.
+     *
+     * @param evt A PropertyChangeEvent object describing the event source
+     *            and the property that has changed.
+     */
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        Object state = evt.getNewValue();
+        if (state instanceof DashboardState) {
+            DashboardState dashboardState = (DashboardState) evt.getNewValue();
+            for (AccountInfo account: dashboardState.getAccounts() ){
+                defaultTableModel.addRow(new Object[]{account.getIconURL(), account.getTitle(), account.getURL(), account.getDate()});
+            }
+        }
+
+    }
+
+
 }
