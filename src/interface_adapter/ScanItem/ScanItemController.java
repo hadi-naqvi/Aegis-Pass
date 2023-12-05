b8fd54ae7815ef5cfc1d6b8819e54d6eb8361b22
azlannaeem <97074156+azlannaeem@users.noreply.github.com>
2023-12-05 12:51:49 -0500

    implemented ScanItem controller

---
+++ src/interface_adapter/ScanItem/ScanItemController.java
@@ -0,0 +1,32 @@
+package interface_adapter.ScanItem;
+
+import use_case.ScanItem.ScanItemInputBoundary;
+import use_case.ScanItem.ScanItemInputData;
+
+public class ScanItemController {
+    public final ScanItemInputBoundary scanItemInteractor;
+
+    /**
+     * Constructor method for the controller for the authentication use case
+     * @param scanItemInteractor The use case interactor object for the scanItem use case
+     */
+    public ScanItemController(ScanItemInputBoundary scanItemInteractor) {
+        this.scanItemInteractor = scanItemInteractor;
+    }
+
+    /**
+     * Method which is executed when the user scans a file
+     */
+    public void scanFile(String filePath) {
+        ScanItemInputData scanItemInputData = new ScanItemInputData(filePath, "");
+        scanItemInteractor.scanFile(scanItemInputData);
+    }
+
+    /**
+     * Method which is executed when the user scans a URL
+     */
+    public void scanUrl(String url) {
+        ScanItemInputData scanItemInputData = new ScanItemInputData("", url);
+        scanItemInteractor.scanUrl(scanItemInputData);
+    }
+}
