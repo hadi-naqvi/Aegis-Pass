9b0fb683e2001266dbcf952b04f8885c7396864c
Josh <josh.joseph@mail.utoronto.ca>
2023-12-04 22:49:06 -0500

    Implemented Input and Output Boundary for the CreateAccount use case

--- src/use_case/CreateAccount/CreateAccountInputBoundary.java
+++ src/use_case/CreateAccount/CreateAccountInputBoundary.java
@@ -1,4 +1,11 @@
 package use_case.CreateAccount;
 
 public interface CreateAccountInputBoundary {
+    /**
+     * Method which contains the logic for the CreateAccount use case (interactor) which is triggered to complete use case
+     * @param createAccountInputData The input data for the use case interactor
+     */
+    void execute(CreateAccountInputData createAccountInputData);
+
+    void switchView();
 }
--- src/use_case/CreateAccount/CreateAccountOutputBoundary.java
+++ src/use_case/CreateAccount/CreateAccountOutputBoundary.java
@@ -1,4 +1,17 @@
 package use_case.CreateAccount;
 
 public interface CreateAccountOutputBoundary {
+    /**
+     * Method which updates the view manager model to display the Dashboard screen after CreateAccount is complete
+     * @param createAccountOutputData The SetupAuth use case output data
+     */
+    void prepareSuccessView(CreateAccountOutputData createAccountOutputData);
+
+    /**
+     * Method which updates the view manager model to display the failed CreateAccount screen
+     * @param error The error message which explains why CreateAccount failed
+     */
+    void prepareFailView(String error);
+
+    void switchView();
 }
