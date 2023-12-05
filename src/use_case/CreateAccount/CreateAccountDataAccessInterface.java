261e9ea76da518b21f8d71409311f3004bbf4792
Josh <josh.joseph@mail.utoronto.ca>
2023-12-04 22:51:47 -0500

    Implemented DAI for CreateAccount use case(addAcount method)

--- src/use_case/CreateAccount/CreateAccountDataAccessInterface.java
+++ src/use_case/CreateAccount/CreateAccountDataAccessInterface.java
@@ -1,4 +1,18 @@
 package use_case.CreateAccount;
 
+import entity.AccountInfo;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface CreateAccountDataAccessInterface {
+
+    /**
+     * Method gets all the currently signed in user's accounts
+     * @return returns list of user's accounts
+     */
+    List<AccountInfo> getAccounts();
+
+    void addAccount(String title, String username, String password, String secret, String url,
+                    String iconURL, String date, String notes);
 }
