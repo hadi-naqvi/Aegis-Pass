814531438d553d999dc8bb02899697322940a4c0
Josh <josh.joseph@mail.utoronto.ca>
2023-12-05 11:59:17 -0500

    Implemented input and output boundary for updateAccount use case

--- src/use_case/UpdateAccount/UpdateAccountOutputBoundary.java
+++ src/use_case/UpdateAccount/UpdateAccountOutputBoundary.java
@@ -1,4 +1,19 @@
 package use_case.UpdateAccount;
 
+import use_case.CreateAccount.CreateAccountOutputData;
+
 public interface UpdateAccountOutputBoundary {
+    /**
+     * Method which updates the view manager model to display the Dashboard screen after UpdateAccount is complete
+     * @param updateAccoutOutputData The updateAccount use case output data
+     */
+    void prepareSuccessView(UpdateAccoutOutputData updateAccoutOutputData);
+
+    /**
+     * Method which updates the view manager model to display the failed UpdateAccount screen
+     * @param error The error message which explains why UpdateAccount failed
+     */
+    void prepareFailView(String error);
+
+    void switchView();
 }
