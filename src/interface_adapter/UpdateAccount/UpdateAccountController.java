bfb3414f6553301a7b616beb60b1321ca1f29dea
Josh <josh.joseph@mail.utoronto.ca>
2023-12-05 12:34:48 -0500

    Implemented controller and presenter for UpdateAccount use case

--- src/interface_adapter/UpdateAccount/UpdateAccountController.java
+++ src/interface_adapter/UpdateAccount/UpdateAccountController.java
@@ -1,4 +1,34 @@
 package interface_adapter.UpdateAccount;
 
+import use_case.UpdateAccount.UpdateAccountInputBoundary;
+import use_case.UpdateAccount.UpdateAccountInputData;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class UpdateAccountController {
+    public final UpdateAccountInputBoundary updateAccountInteractor;
+
+    /**
+     * Constructor method for the controller for the updateAccount use case
+     * @param updateAccountInteractor The use case interactor object for the updateAccount use case
+     */
+    public UpdateAccountController(UpdateAccountInputBoundary updateAccountInteractor) {
+        this.updateAccountInteractor = updateAccountInteractor;
+    }
+
+    /**
+     * Method which is executed/triggered when the user enters the updateAccount view
+     */
+    public void execute(int accountIndex, String title, String username, String password, String secretKey, String url,
+                        String iconURL, LocalDateTime date, String notes) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        UpdateAccountInputData updateAccountInputData = new UpdateAccountInputData(accountIndex, title, username, password,
+                secretKey, url, iconURL, formatter.format(date), notes);
+        updateAccountInteractor.execute(updateAccountInputData);
+    }
+
+    public void switchView(){
+        updateAccountInteractor.switchView();
+    }
 }
--- src/interface_adapter/UpdateAccount/UpdateAccountPresenter.java
+++ src/interface_adapter/UpdateAccount/UpdateAccountPresenter.java
@@ -1,4 +1,58 @@
 package interface_adapter.UpdateAccount;
 
-public class UpdateAccountPresenter {
+import interface_adapter.Dashboard.DashboardState;
+import interface_adapter.Dashboard.DashboardViewModel;
+import interface_adapter.ViewManagerModel;
+import use_case.UpdateAccount.UpdateAccountOutputBoundary;
+import use_case.UpdateAccount.UpdateAccountOutputData;
+
+public class UpdateAccountPresenter implements UpdateAccountOutputBoundary {
+    private final UpdateAccountViewModel updateAccountViewModel;
+    private final DashboardViewModel dashboardViewModel;
+    private final ViewManagerModel viewManagerModel;
+
+
+    /**
+     * Constructor method for the UpdateAccount use case's presenter
+     * @param updateAccountViewModel The view model for the UpdateAccount view
+     * @param dashboardViewModel The view model for the dashboard view
+     */
+    public UpdateAccountPresenter(ViewManagerModel viewManagerModel,
+                                  UpdateAccountViewModel updateAccountViewModel,
+                                  DashboardViewModel dashboardViewModel) {
+        this.updateAccountViewModel = updateAccountViewModel;
+        this.dashboardViewModel = dashboardViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+
+    /**
+     * Method which updates the view manager model to display the dashboard screen after updateAccount is complete
+     * @param updateAccountOutputData The UpdateAccount use case output data
+     */
+    @Override
+    public void prepareSuccessView(UpdateAccountOutputData updateAccountOutputData) {
+        DashboardState dashboardState = dashboardViewModel.getState();
+        dashboardState.setAccounts(updateAccountOutputData.getAccounts());
+        dashboardViewModel.setState(dashboardState);
+
+        switchView();
+    }
+
+    /**
+     * Method which updates the view manager model to display the failed updateAccount screen
+     * @param error The error message which explains why updating an account failed
+     */
+    @Override
+    public void prepareFailView(String error) {
+        UpdateAccountState updateAccountState = updateAccountViewModel.getState();
+        updateAccountState.setUsernameError(error);
+        this.updateAccountViewModel.firePropertyChanged();
+    }
+
+    public void switchView(){
+        DashboardState dashboardState = dashboardViewModel.getState();
+        dashboardState.setRightPanelView("dashboard");
+        dashboardViewModel.setState(dashboardState);
+        this.dashboardViewModel.firePropertyChanged();
+    }
 }
