e4cb04884dcd8008789954f4782f3220403736c3
Alex Macri <alexmacri04@gmail.com>
2023-12-05 16:40:14 -0500

    added code for the Input Boundary for the Delete Account use case

--- src/use_case/DeleteAccount/DeleteAccountInputBoundary.java
+++ src/use_case/DeleteAccount/DeleteAccountInputBoundary.java
@@ -1,2 +1,11 @@
-package use_case.DeleteAccount;public class DeleteAccountInputBoundary {
+package use_case.DeleteAccount;
+
+public interface DeleteAccountInputBoundary {
+
+    /**
+     * Method which contains the logic for the Delete Account use case (interactor) which is triggered to complete use case
+     * @param deleteAccountInputData The input data for the use case interactor
+     */
+    void execute(DeleteAccountInputData deleteAccountInputData);
+
 }
