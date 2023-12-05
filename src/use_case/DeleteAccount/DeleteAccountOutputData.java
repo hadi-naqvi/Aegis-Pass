95d91535d07db598f8018dd4b3ad2bed66a273c5
Alex Macri <alexmacri04@gmail.com>
2023-12-05 18:31:33 -0500

    wrote the Output Data class for the Delete Account use case

--- src/use_case/DeleteAccount/DeleteAccountOutputData.java
+++ src/use_case/DeleteAccount/DeleteAccountOutputData.java
@@ -1,4 +1,25 @@
 package use_case.DeleteAccount;
 
-public class DeleteAccountOuputData {
+import entity.AccountInfo;
+
+import java.util.List;
+
+public class DeleteAccountOutputData {
+
+    private final boolean useCaseFailed;
+    private List<AccountInfo> accounts;
+
+    /**
+     * Constructor method for the Delete Account output data
+     * @param useCaseFailed Whether the use case failed or not (i.e. failed deleting account)
+     */
+    public DeleteAccountOutputData(boolean useCaseFailed, List<AccountInfo> accounts) {
+        this.useCaseFailed = useCaseFailed;
+        this.accounts = accounts;
+    }
+
+    public List<AccountInfo> getAccounts(){
+        return this.accounts;
+    }
+
 }
