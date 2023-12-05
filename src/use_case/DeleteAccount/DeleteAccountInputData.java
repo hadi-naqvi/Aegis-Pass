c1beae069489c918f2357bf61ddc00652b2237d3
Alex Macri <alexmacri04@gmail.com>
2023-12-05 17:00:29 -0500

    added code for the Input Data for the Delete Input use case

--- src/use_case/DeleteAccount/DeleteAccountInputData.java
+++ src/use_case/DeleteAccount/DeleteAccountInputData.java
@@ -1,2 +1,33 @@
-package use_case.DeleteAccount;public class DeleteAccountInputData {
+package use_case.DeleteAccount;
+
+public class DeleteAccountInputData {
+
+    final private String title;
+    final private String username;
+
+    /**
+     * Constructor for the Delete Account use case's input data
+     * @param title of the account to be deleted
+     * @param username of the account to be deleted
+     */
+    public DeleteAccountInputData(String title, String username) {
+        this.title = title;
+        this.username = username;
+    }
+
+    /**
+     * Getter method for the title of the account to be deleted
+     * @return The username of the account.
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Getter method for the username of the account to be deleted
+     * @return The username of the account.
+     */
+    public String getUsername() {
+        return username;
+    }
 }
