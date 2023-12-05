45c15352c319038c095a08a5b722a51306ade87a
hadi-naqvi <92334527+hadi-naqvi@users.noreply.github.com>
2023-12-05 16:50:59 -0500

    Implemented the input and output boundaries for generate password use case

--- src/use_case/GeneratePassword/GeneratePasswordInputBoundary.java
+++ src/use_case/GeneratePassword/GeneratePasswordInputBoundary.java
@@ -1,4 +1,11 @@
 package use_case.GeneratePassword;
 
+import use_case.Dashboard.DashboardInputData;
+
 public interface GeneratePasswordInputBoundary {
+    /**
+     * Method which contains the logic for the Generate Password use case (interactor) which is triggered to complete use case
+     * @param generatePasswordInputData The input data for the use case interactor
+     */
+    void execute(DashboardInputData generatePasswordInputData);
 }
--- src/use_case/GeneratePassword/GeneratePasswordOutputBoundary.java
+++ src/use_case/GeneratePassword/GeneratePasswordOutputBoundary.java
@@ -1,4 +1,17 @@
 package use_case.GeneratePassword;
 
+import use_case.Dashboard.DashboardOutputData;
+
 public interface GeneratePasswordOutputBoundary {
+    /**
+     * Method for the success view of the generate password use case
+     * @param generatePasswordOutputData The generate password use case's output data
+     */
+    void prepareSuccessView(GeneratePasswordOutputData generatePasswordOutputData);
+
+    /**
+     * Method for the fail view of the generate password use case
+     * @param error The error message of the generate password use case
+     */
+    void prepareFailView(String error);
 }
