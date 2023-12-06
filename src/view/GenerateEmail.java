1bcf96dae4a6252c2ae21faa7b1233226303e77d
azlannaeem <97074156+azlannaeem@users.noreply.github.com>
2023-12-06 16:50:43 -0500

    implemented GenerateView and corresponding GUI form

---
+++ src/view/GenerateEmail.java
@@ -0,0 +1,98 @@
+package view;
+
+import interface_adapter.CreateAccount.CreateAccountState;
+import interface_adapter.Dashboard.DashboardViewModel;
+import interface_adapter.GenerateEmail.GenerateEmailController;
+import interface_adapter.GenerateEmail.GenerateEmailState;
+import interface_adapter.GenerateEmail.GenerateEmailViewModel;
+
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class GenerateEmail extends JPanel implements ActionListener, PropertyChangeListener {
+    public final String viewName = "generate email";
+    private final GenerateEmailViewModel generateEmailViewModel;
+    private final GenerateEmailController generateEmailController;
+    private final DashboardViewModel dashboardViewModel;
+    private JPanel genEmailRightPanel;
+    private JPanel genEmailPanel;
+    private JButton backButton;
+    private JButton generateEmailButton;
+    private JTextField emailTextfield;
+    private JButton copyButton;
+    private JPanel main;
+
+    public GenerateEmail(GenerateEmailViewModel generateEmailViewModel, GenerateEmailController generateEmailController,
+                         DashboardViewModel dashboardViewModel) {
+        this.generateEmailViewModel = generateEmailViewModel;
+        this.generateEmailController = generateEmailController;
+        this.dashboardViewModel = dashboardViewModel;
+        this.generateEmailViewModel.addPropertyChangeListener(this);
+
+
+        generateEmailButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (e.getSource().equals(generateEmailController)) {
+                    GenerateEmailState currentState = generateEmailViewModel.getState();
+                    generateEmailController.execute(currentState.getAccountName());
+                }
+            }
+        });
+
+        emailTextfield.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                updateState();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                updateState();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                // Plain text components don't fire these events
+            }
+
+            private void updateState() {
+                GenerateEmailState currentState = generateEmailViewModel.getState();
+                currentState.setAccountName(emailTextfield.getText());
+                generateEmailViewModel.setState(currentState);
+            }
+        });
+
+
+        this.setLayout(new GridLayout());
+        this.add(main);
+    }
+
+
+    /**
+     * Invoked when an action occurs.
+     *
+     * @param e the event to be processed
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    /**
+     * This method gets called when a bound property is changed.
+     *
+     * @param evt A PropertyChangeEvent object describing the event source
+     *            and the property that has changed.
+     */
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
+}
