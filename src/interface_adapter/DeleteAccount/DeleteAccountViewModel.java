89d04a4a9fd22399023ce1af364adecc37383047
Alex Macri <alexmacri04@gmail.com>
2023-12-05 21:26:59 -0500

    wrote the View Model for the Delete Account use case

--- src/interface_adapter/DeleteAccount/DeleteAccountViewModel.java
+++ src/interface_adapter/DeleteAccount/DeleteAccountViewModel.java
@@ -1,2 +1,52 @@
-package interface_adapter.DeleteAccount;public class DeleteAccountViewModel {
+package interface_adapter.DeleteAccount;
+
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class DeleteAccountViewModel extends ViewModel {
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+    private DeleteAccountState state = new DeleteAccountState();
+
+    /**
+     * Constructor method for Delete Account use case
+     */
+    public DeleteAccountViewModel() {
+        super("delete account");
+    }
+
+    /**
+     * method which triggers/executes when the state changes
+     */
+    @Override
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    /**
+     * Method which adds a PropertyChangeListener to the view model
+     * @param listener The PropertyChangeListener to be added
+     */
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    /**
+     * Getter method for the Delete Account view model's state
+     * @return the Delete Account view model's state
+     */
+    public DeleteAccountState getState() {
+        return this.state;
+    }
+
+    /**
+     * Setter method for the Delete Account view model's state
+     * @param state the Delete Account view model's state
+     */
+    public void setState(DeleteAccountState state) {
+        this.state = state;
+    }
 }
