43aeaa005f72a0ab31a0a520e59d7bfe608c401c
Alex Macri <alexmacri04@gmail.com>
2023-12-06 16:40:18 -0500

    made the form for the Check Breach View for the Check Breach use case

---
+++ src/view/CheckBreachView.java
@@ -0,0 +1,15 @@
+package view;
+
+import javax.swing.*;
+
+public class CheckBreachView {
+    private JPanel main;
+    private JPanel breachRightPanel;
+    private JPanel breachPanel;
+    private JRadioButton checkEmailRadioButton;
+    private JRadioButton checkPasswordRadioButton;
+    private JTextField typePasswordTextField;
+    private JButton confirmPasswordButton;
+    private JTextField typeEmailTextField;
+    private JButton confirmEmailButton;
+}
