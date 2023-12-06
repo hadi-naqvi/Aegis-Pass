c9e872a12c146638ffe873fb420128a8cdf86582
Alex Macri <alexmacri04@gmail.com>
2023-12-06 18:01:34 -0500

    added the Interactor for the Check breach use case

---
+++ src/use_case/CheckBreach/CheckBreachInteractor.java
@@ -0,0 +1,67 @@
+package use_case.CheckBreach;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class CheckBreachInteractor implements CheckBreachInputBoundary {
+    
+    private CheckBreachDataAccessInterface checkBreachDataAccessObject;
+    private CheckBreachOutputBoundary checkBreachPresenter;
+
+    /**
+     * Constructor for the Interactor of the Check Breach use case
+     * @param dataAccessObject the input data for the Check Breach use case
+     * @param presenter the presenter for the Check Breach use case
+     */
+    public CheckBreachInteractor(CheckBreachDataAccessInterface dataAccessObject, CheckBreachOutputBoundary presenter) {
+        this.checkBreachDataAccessObject = dataAccessObject;
+        this.checkBreachPresenter = presenter;
+    }
+    
+    /**
+     * Method which contains the logic for checking if an email has been breached for the Check Breach use case
+     *
+     * @param checkBreachInputData the input data for the use case interactor
+     */
+    @Override
+    public void checkEmail(CheckBreachInputData checkBreachInputData) throws IOException, InterruptedException {
+        try {
+            // perform business logic, interact with data access
+            CheckBreachOutputData outputData = new CheckBreachOutputData(false,
+                    checkBreachDataAccessObject.checkEmail(checkBreachInputData.getEmail()));
+            checkBreachPresenter.prepareSuccessView(outputData);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException | URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Method which contains the logic for checking if a password has been breached for the Check Breach use case
+     *
+     * @param checkBreachInputData the input data for the use case interactor
+     */
+    @Override
+    public void checkPassword(CheckBreachInputData checkBreachInputData) throws IOException, InterruptedException {
+        try {
+            // perform business logic, interact with data access
+            CheckBreachOutputData outputData = new CheckBreachOutputData(false,
+                    checkBreachDataAccessObject.checkPassword(checkBreachInputData.getPassword()));
+            checkBreachPresenter.prepareSuccessView(outputData);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException | URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Method that tells the presenter to switch to the Dashboard View
+     */
+    @Override
+    public void switchView() {
+        checkBreachPresenter.switchView();
+    }
+    
+}
