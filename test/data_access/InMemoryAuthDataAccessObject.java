1fb0c2e40cc8482ff02d56c5fac26ce5e55e4406
Alex Macri <alexmacri04@gmail.com>
2023-12-07 06:12:32 -0500

    Updated the Auth Data In Memory Data Access Object to accomodate for the new methods when implementing AuthenticationDataAccessInterface, and added support for what represents the rest of the columns in the MySQL table that this represents (has a UserID, salt, and the password is now stored encrypted)

--- test/data_access/InMemoryAuthDataAccessObject.java
+++ test/data_access/InMemoryAuthDataAccessObject.java
@@ -1,14 +1,18 @@
 package data_access;
 
 import entity.User;
+import org.mindrot.jbcrypt.BCrypt;
+import use_case.Authentication.AuthenticationDataAccessInterface;
 import use_case.SetupAuth.SetupAuthDataAccessInterface;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.security.SecureRandom;
+import java.util.*;
 
-public class InMemoryAuthDataAccessObject implements SetupAuthDataAccessInterface {
+public class InMemoryAuthDataAccessObject implements SetupAuthDataAccessInterface, AuthenticationDataAccessInterface {
 
-    private final Map<String, User> users = new HashMap<>();
+    private final Map<String, List<String>> users = new HashMap<>();
+    private final String PEPPER = "$2a$15$sDwsCyD.ZiWm3zSh1lzR0e";
+    private final String SALT = "$2a$15$sDwsCyD.ZiWm3zSh1lzR0e";
 
     /**
      * Method which saves the user's username and password to our "fake database"
@@ -17,7 +21,10 @@ public class InMemoryAuthDataAccessObject implements SetupAuthDataAccessInterfac
      */
     @Override
     public void save(User user) {
-        users.put(user.getUsername(), user);
+        String userId = String.valueOf(this.users.size() + 1);
+        String encryptedPassword = BCrypt.hashpw(user.getPassword() + this.PEPPER, this.SALT);
+        String[] userData = {userId, encryptedPassword, SALT};
+        users.put(user.getUsername(), new ArrayList<>(Arrays.asList(userData)));
     }
 
     /**
@@ -28,6 +35,41 @@ public class InMemoryAuthDataAccessObject implements SetupAuthDataAccessInterfac
     @Override
     public boolean existsByName(String username) {
         return users.containsKey(username);
+    }
+
+    /**
+     * Method validates the user's password with their corresponding username
+     *
+     * @param username The user's inputted username
+     * @param password The user's inputted password
+     * @return returns whether password matches username
+     */
+    @Override
+    public boolean validate(String username, String password) {
+        String encryptedPassword = BCrypt.hashpw(password + this.PEPPER, this.SALT);
+        return encryptedPassword.equals(users.get(username).get(1));
+    }
+
+    /**
+     * Method which returns a username's corresponding user ID in the database
+     *
+     * @param username The username
+     * @return The user's corresponding user ID
+     */
+    @Override
+    public int getUserID(String username) {
+        return Integer.parseInt(users.get(username).get(0));
+    }
+
+    /**
+     * Method which returns a username's corresponding kdf salt in the database
+     *
+     * @param username The username
+     * @return The user's corresponding kdf salt
+     */
+    @Override
+    public String getUserSalt(String username) {
+        return users.get(username).get(2);
     }
 
 }
