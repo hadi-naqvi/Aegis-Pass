09dc06d7a862efe9a836972f19b110f81e89f74f
Josh <josh.joseph@mail.utoronto.ca>
2023-12-07 01:23:09 -0500

    Implemented controller, presenter, state, and view model for Generate2FACode use case

--- src/interface_adapter/Generate2FACode/Generate2FACodeController.java
+++ src/interface_adapter/Generate2FACode/Generate2FACodeController.java
@@ -1,4 +1,26 @@
 package interface_adapter.Generate2FACode;
 
+
+import use_case.Generate2FACode.Generate2FACodeInputBoundary;
+import use_case.Generate2FACode.Generate2FACodeInputData;
+
+
 public class Generate2FACodeController {
+    public final Generate2FACodeInputBoundary generate2FACodeInteractor;
+
+    /**
+     * Constructor method for the controller for the generate2FACode use case
+     * @param generate2FACodeInteractor The use case interactor object for the generate2FACode use case
+     */
+    public Generate2FACodeController(Generate2FACodeInputBoundary generate2FACodeInteractor) {
+        this.generate2FACodeInteractor = generate2FACodeInteractor;
+    }
+
+    /**
+     * Method which is executed/triggered when the user enters the CardView
+     */
+    public void execute(String secretKey) {
+        Generate2FACodeInputData generate2FACodeInputData = new Generate2FACodeInputData(secretKey);
+        generate2FACodeInteractor.execute(generate2FACodeInputData);
+    }
 }
--- src/interface_adapter/Generate2FACode/Generate2FACodeViewModel.java
+++ src/interface_adapter/Generate2FACode/Generate2FACodeViewModel.java
@@ -1,4 +1,52 @@
 package interface_adapter.Generate2FACode;
 
-public class Generate2FACodeViewModel {
+import interface_adapter.CreateAccount.CreateAccountState;
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class Generate2FACodeViewModel extends ViewModel {
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+    private Generate2FACodeState state = new Generate2FACodeState();
+
+    /**
+     * Constructor method for the Generate2FACode view's view model
+     */
+    public Generate2FACodeViewModel() {
+        super("dashboard");
+    }
+
+    /**
+     * method which triggers/executes when the state changes
+     */
+    @Override
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    /**
+     * Method which adds a PropertyChangeListener to the view model
+     * @param listener The PropertyChangeListener to be added
+     */
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    /**
+     * Getter method for the Generate2FACode view model's state
+     * @return The Generate2FACode view model's state
+     */
+    public Generate2FACodeState getState() {
+        return this.state;
+    }
+
+    /**
+     * Setter method for the Generate2FACode view model's state
+     * @param generate2FACodeState The state to be set
+     */
+    public void setState(Generate2FACodeState generate2FACodeState) {
+        this.state = generate2FACodeState;
+    }
 }
